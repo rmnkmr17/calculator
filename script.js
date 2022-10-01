b15939e5f38f3406bc75121939748868448b9c3e
rmkkmr17 <ramkum94@tutanota.com>
2022-10-01 15:38:18 +0200

    Revised some old functions. Added decimal support

--- script.js
+++ script.js
@@ -14,33 +14,73 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, num1, num2) {
+const calculator = {
+    displayValue: "0",
+    num1: null,
+    expectNum2: false,
+    operator: null,
+};
+
+function inputNumber(num) {
+    const { displayValue } = calculator;
+    calculator.displayValue = displayValue === "0" ? num : displayValue + num;
+}
+
+function inputDecimal(dot) {
+    if (!calculator.displayValue.includes(dot)) {
+        calculator.displayValue += dot;
+    }
+}
+
+function operate(num1, num2, operator) {
     switch (operator) {
         case "+":
             return add(num1, num2);
+            break;
         case "-":
             return subtract(num1, num2);
+            break;
         case "*":
             return multiply(num1, num2);
+            break;
         case "/":
             return divide(num1, num2);
+            break;
+        default:
+            return num2;
     }
 };
 
-// Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step.
-
-let displayValue = "";
-let display = document.querySelector(".display");
-
-
-let numbers = document.querySelectorAll(".number");
-numbers.forEach((number) => number.addEventListener("click", function(e){
-    showNumber(e.target.textContent);
-    display.textContent = displayValue;
-}));
-
-function showNumber(num) {
-    if (displayValue.length <= 5) {
-        displayValue += num;
-    }
+function updateDisplay() {
+    const display = document.querySelector(".display");
+    display.value = calculator.displayValue;
 }
+
+updateDisplay();
+
+const buttons = document.querySelector(".buttons");
+buttons.addEventListener("click", (event) => {
+    const { target } = event;
+    if (!target.matches("button")) {
+        return;
+    }
+
+    if (target.classList.contains("operator")) {
+        console.log("operator", target.value);
+        return;
+    }
+
+    if (target.classList.contains("decimal")) {
+        inputDecimal(target.value);
+        updateDisplay();
+        return;
+    }
+
+    if (target.classList.contains("clear")) {
+        console.log("clear", target.value);
+        return;
+    }
+
+    inputNumber(target.value);
+    updateDisplay();
+})
